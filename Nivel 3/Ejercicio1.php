ee173851749da0488d08785de2c636d89952077e
francobrida <francobrida@gmail.com>
2025-12-04 10:30:43 +0100

    Added commentary on Nivel 3 Ejercicio1

--- Nivel 3/Ejercicio1.php
+++ Nivel 3/Ejercicio1.php
@@ -1,11 +1,11 @@
 <?php
 
 function primeNumbers ($limit) {
-    $isPrime = array_fill(0, $limit + 1, true); // Creates an array to track prime status, each number has a boolean value
+    $isPrime = array_fill(0, $limit + 1, true); // Creates an array to set status by assigning each number a boolean value
         $isPrime[0] = false;
         $isPrime[1] = false;
 
-        for ($i = 2; $i * $i <= $limit; $i++) { 
+        for ($i = 2; $i * $i <= $limit; $i++) { // I used some AI to help me with the maths on this loop
             if ($isPrime[$i]) {
                 for ($multiple = $i * $i; $multiple <= $limit; $multiple += $i) {
                  $isPrime[$multiple] = false;
@@ -13,7 +13,7 @@ function primeNumbers ($limit) {
             }
         }
 
-        $primes = [];
+        $primes = []; // New array to save only the prime numbers (previously set on true)
         for ($j = 2; $j <= $limit; $j++) {
             if ($isPrime[$j]) {
                 $primes[] = $j;
