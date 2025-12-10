9fada705bd0bc98e88a0a3459aa3915d5a9d89da
francobrida <francobrida@gmail.com>
2025-12-10 13:18:34 +0100

    Commented Nivel 3 Ejercicio1 to re-do later

--- Nivel 3/Ejercicio1.php
+++ Nivel 3/Ejercicio1.php
@@ -1,9 +1,13 @@
 <?php
 
+
+
 function calculatePrimeNumbers($limit) {
     $isPrime = array_fill(0, $limit + 1, true); // Creates an array to set status by assigning each number a boolean value
         $isPrime[0] = false;
         $isPrime[1] = false;
+
+        /* TO RE-DO WITHOUT ANY AI
 
         for ($i = 2; $i * $i <= $limit; $i++) { 
             if ($isPrime[$i]) {
@@ -20,6 +24,7 @@ function calculatePrimeNumbers($limit) {
             }
         }
     return $primes; // Returns array with prime numbers up to the limit
+    */
 }
 
 ?>
